4581577168b1159cb9e9869345103d0b239bd1b9
Ashish Verma <ashishkv.2016@gmail.com>
2021-05-22 00:17:01 +0530

    added login feature

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const mongoose=require('mongoose')
 const needyRoute=require('./routes/needyRoute')
 const donarRoute=require('./routes/donarRoute')
 const volunteerRoute=require('./routes/volunteerRoute')
+const authRoute=require('./routes/authRoute')
+require("dotenv").config();
+const cookieParser=require('cookie-parser')
+const { requireAuth, checkUser } = require('./middleware/authMiddleware')
 
-const dbUri='mongodb+srv://Ashish:ashishkv2016@cluster0.hxqys.mongodb.net/DevJam?retryWrites=true&w=majority'
+const dbUri=process.env.MONGO_URI;
 mongoose.connect(dbUri,{useNewUrlParser:true, useUnifiedTopology:true})
-    .then((result)=>app.listen(3000))
+    .then((result)=>{app.listen(8000); console.log('connected to db and started')})
     .catch((err)=>{
         console.log(err)
     })
@@ -16,14 +20,19 @@ mongoose.connect(dbUri,{useNewUrlParser:true, useUnifiedTopology:true})
 app.use(express.static('./public'))
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cookieParser())
 
+app.get('*',checkUser)
 app.get('/',(req,res)=>{
     res.render('index')
 })
 
-app.use('/needy',needyRoute)
-app.use('/donar', donarRoute)
-app.use('/volunteer', volunteerRoute)
+app.use('/needy',requireAuth,needyRoute)
+app.use('/donar',requireAuth, donarRoute)
+app.use('/volunteer',requireAuth, volunteerRoute)
+app.get('/main',requireAuth,(req,res)=>res.render('main'))
+app.use(authRoute)
 
 app.use((req,res)=>{
     res.status(404).render('404',{title:404})
